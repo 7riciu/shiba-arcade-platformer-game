1f6d3c6dda9dec09d8dadea32112c8ea575e3c54
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-09-21 03:28:00 +0300

    I added 4 more sprites and a killzone

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta

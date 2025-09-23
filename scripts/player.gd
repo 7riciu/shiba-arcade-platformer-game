9c3c374a25e05543adef7df30d76991558ab6bac
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-09-23 16:49:56 +0300

    The player moves smoothly now

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,41 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+@export var speed : int = 150
+@export_range(0, 1) var deceleration = 0.1
+@export_range(0, 1) var acceleration = 0.1
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@export var jump_force : int = -300
+@export_range(0, 1) var decelerate_jump = 0.5
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+@export var gravity : int = 800
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+@onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+
+	# Move left-right
+	var direction = Input.get_axis("Left", "Right")
+
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
+
+	# Flip around
+	if direction == 1:
+		player_sprite.flip_h = false
+	elif direction == -1:
+		player_sprite.flip_h = true
+
+	# Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = jump_force
+	
+	if Input.is_action_just_released("Jump") and velocity.y < 0:
+		velocity.y = jump_force * decelerate_jump
 
 	move_and_slide()
